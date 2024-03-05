50435e41c997b352d60911eccad1bf3df16de4f6
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-03-05 20:32:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Specify a globally unique bucket name
+  bucket = "my-unique-kmit-name"  # Specify a globally unique bucket name
 
   acl    = "private"  # Access Control List for the bucket, can be adjusted based on your needs
 
