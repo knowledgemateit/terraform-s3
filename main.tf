2072f7b55dacfd2a611bb84b54dbfd07244e70b8
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2024-06-26 20:45:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region = "ap-south-1"  # Specify your desired AWS region
+  region = "us-east-1"  # Specify your desired AWS region
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-kmit-name"  # Specify a globally unique bucket name
+  bucket = "my-unique-avan-name"  # Specify a globally unique bucket name
 
   acl    = "public-read"  # Access Control List for the bucket, can be adjusted based on your needs
 
   tags = {
-    Name        = "MyS3Bucket"
+    Name        = "my-unique-avan-name"
     Environment = "Production"
   }
 }
